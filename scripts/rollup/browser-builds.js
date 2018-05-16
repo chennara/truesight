59b7228a1cbcb03591e31695e3059c7ff6e18b9b
Chennara Nhes <chennara.nhes@gmail.com>
2018-05-16 10:12:04 +0200

    Use object destructuring in Rollup build configuration

--- scripts/rollup/browser-builds.js
+++ scripts/rollup/browser-builds.js
@@ -2,68 +2,61 @@ import path from 'path';
 
 import { version } from '../../packages/truesight/package.json';
 
+const TRUESIGHT_BUNDLE_NAME = 'truesight';
+
 const CJS_BUILD = 'cjs';
 const ESM_BUILD = 'esm';
 const UMD_DEVELOPMENT_BUILD = 'umd';
 const UMD_PRODUCTION_BUILD = 'umd-prod';
 
 const root = path.resolve('packages/truesight');
-const input = path.join(root, 'src/index.js');
-const output = {
+
+const baseEntry = {
+  root,
+  input: path.join(root, 'src/index.js'),
+};
+
+const outputEntry = {
   directory: path.join(root, 'dist'),
   version,
 };
 
-const TRUESIGHT_BUNDLE_NAME = 'truesight';
-
 const browserBuilds = {
   [CJS_BUILD]: {
-    root,
-    input,
-    output: Object.assign(
-      {
-        file: `${TRUESIGHT_BUNDLE_NAME}.common.js`,
-        format: 'cjs',
-        uglify: false,
-      },
-      output
-    ),
+    ...baseEntry,
+    output: {
+      ...outputEntry,
+      file: `${TRUESIGHT_BUNDLE_NAME}.common.js`,
+      format: 'cjs',
+      uglify: false,
+    },
   },
   [ESM_BUILD]: {
-    root,
-    input,
-    output: Object.assign(
-      {
-        file: `${TRUESIGHT_BUNDLE_NAME}.esm.js`,
-        format: 'es',
-        uglify: false,
-      },
-      output
-    ),
+    ...baseEntry,
+    output: {
+      ...outputEntry,
+      file: `${TRUESIGHT_BUNDLE_NAME}.esm.js`,
+      format: 'es',
+      uglify: false,
+    },
   },
   [UMD_DEVELOPMENT_BUILD]: {
-    root,
-    input,
-    output: Object.assign(
-      {
-        file: `${TRUESIGHT_BUNDLE_NAME}.js`,
-        format: 'umd',
-        uglify: false,
-      },
-      output
-    ),
+    ...baseEntry,
+    output: {
+      ...outputEntry,
+      file: `${TRUESIGHT_BUNDLE_NAME}.js`,
+      format: 'umd',
+      uglify: false,
+    },
   },
   [UMD_PRODUCTION_BUILD]: {
-    root,
-    input,
-    output: Object.assign(
-      {
-        file: `${TRUESIGHT_BUNDLE_NAME}.min.js`,
-        format: 'umd',
-        uglify: true,
-      },
-      output
-    ),
+    ...baseEntry,
+    output: {
+      ...outputEntry,
+      file: `${TRUESIGHT_BUNDLE_NAME}.min.js`,
+      format: 'umd',
+      uglify: true,
+    },
   },
 };
 
